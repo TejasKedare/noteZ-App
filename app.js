8aab40bfd166d2bedf2aa05da947a11ab3c408f4
Tejas Kedare <tejaskedare2@gmail.com>
2020-10-31 19:16:36 +0530

    custom path , sunstring and lodash is added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const _ = require("lodash");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +21,19 @@ app.post("/", (req, res) => {
   postz.push(posts);
 
   res.redirect("/");
+});
+
+app.get("/:note", (req, res) => {
+  const notes = _.lowerCase(req.params.note);
+  postz.forEach((post) => {
+    const noteTitle = _.lowerCase(post.title);
+    if (noteTitle === notes) {
+      res.render("page", {
+        note: post.note,
+        title: post.title,
+      });
+    }
+  });
 });
 
 app.listen(3000, () => console.log("The server id running on port 3000"));
